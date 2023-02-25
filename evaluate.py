1f37c2648a3da0b159df89a7550e4c8616596b65
Muhammad Ilham Malik <muh.ilham.malik@gmail.com>
2023-02-25 14:29:34 +0700

    Update evaluate

--- evaluate.py
+++ evaluate.py
@@ -32,19 +32,24 @@ class Evaluator:
         ide_list = []
         caption_list = []
         model.eval()
+
         with torch.no_grad():
             for data in self.dataloader:
                 features, targets, mask, max_length,ides,motion_feat,object_feat= data
+
                 if self.cfg.model_name == 'mean_pooling':
                     if self.decoding_type == 'greedy':
                         cap, cap_txt = model.GreedyDecoding(features.to(self.cfg.device))
+
                     if self.decoding_type == 'beam':
-                        tensor, cap_txt, scores = model.BeamDecoding(features.to(self.cfg.device),return_single=True)
+                        tensor, cap_txt, scores = model.BeamDecoding(features.to(self.cfg.device),
+                                                                     return_single=True)
                     
                 if self.cfg.model_name == 'sa-lstm':
                     if self.decoding_type == 'greedy':
                         cap, cap_txt, _ = model.GreedyDecoding(features.to(self.cfg.device),
-                                                             motion_feat.to(self.cfg.device),)
+                                                             motion_feat.to(self.cfg.device))
+                        
                     if self.decoding_type == 'beam':
                         cap_txt = model.BeamDecoding(features.to(self.cfg.device), 
                                                      motion_feat.to(self.cfg.device),
@@ -62,21 +67,26 @@ class Evaluator:
                         pass #yet to implement
                     else:
                         pass # yet ti implement
+
                 ide_list += ides
                 caption_list += cap_txt
+
         for a in zip(ide_list,caption_list):
             self.prediction_dict[str(a[0])] = [a[1].strip()]
             
-    def evaluate(self,scorer,model,epoch,loss=9999):
+    def evaluate(self, scorer, model, epoch, loss=9999):
         self.prediction_list(model)
-        scores = scorer.score(self.reference_dict,self.prediction_dict)
+
+        scores = scorer.score(self.reference_dict,
+                              self.prediction_dict)
+        
         self.scores[epoch] = scores
         self.losses[epoch] = loss
+
         return scores
 
-
-
-    def save_model(self,model,epoch):
+    def save_model(self, model, epoch):
         print('Saving models....')
-        filename = os.path.join(self.path.saved_models_path, self.cfg.model_name+str(epoch)+'.pt')
+        filename = os.path.join(self.path.saved_models_path, self.cfg.model_name+str(epoch) + '.pt')
+        
         torch.save(model,filename)
